89682a88b759015918ed2c14bd28f765c1594d2a
ogikun <g1621362@gmail.com>
2022-01-08 01:19:43 +0900

    add checkbox

--- lib/main.dart
+++ lib/main.dart
@@ -7,21 +7,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'ToDo List',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'ToDo List'),
@@ -38,15 +28,31 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  List<String> _todos = [];
+class Todo {
+  String title;
+  bool done;
 
-  String _todo = '';
+  Todo({required this.title, required this.done});
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _todoController = TextEditingController();
+  List<Todo> _todos = [];
 
   void _addTodo() {
-    setState(() {
-      _todos.add(_todo);
-    });
+    if (_todoController.text != '') {
+      final _todo = Todo(title: _todoController.text, done: false);
+      setState(() {
+        _todos.add(_todo);
+        _todoController.clear();
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _todoController.dispose();
+    super.dispose();
   }
 
   @override
@@ -68,23 +74,34 @@ class _MyHomePageState extends State<MyHomePage> {
             Container(
               padding: EdgeInsets.only(right: 20.0, left: 20.0, bottom: 15.0),
               child: TextField(
+                controller: _todoController,
                 decoration: InputDecoration(icon: Icon(Icons.checklist_sharp)),
-                onChanged: (value) {
-                  _todo = value;
-                },
               ),
             ),
             Expanded(
               child: ListView.builder(
                 itemCount: _todos.length,
                 itemBuilder: (BuildContext context, int index) {
+                  final int _reverseIndex = _todos.length - index - 1;
                   return Card(
                     margin: EdgeInsets.all(10.0),
-                    child: Padding(
+                    child: Container(
                       padding: EdgeInsets.all(20.0),
-                      child: Text(
-                        _todos[index],
-                        style: TextStyle(fontSize: 20),
+                      child: Row(
+                        children: <Widget>[
+                          Checkbox(
+                            value: _todos[_reverseIndex].done,
+                            onChanged: (bool? value) {
+                              setState(() {
+                                _todos[_reverseIndex].done = value!;
+                              });
+                            },
+                          ),
+                          Text(
+                            _todos[_reverseIndex].title,
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ],
                       ),
                     ),
                   );
